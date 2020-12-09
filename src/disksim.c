5cbd7a37d2610da072d1c3b995370652897071a9
Derik Dreher <ddreher@unbc.ca>
2020-12-08 18:07:46 -0800

    sure

--- src/disksim.c
+++ src/disksim.c
@@ -5,45 +5,83 @@
 #include "shell.h"
 #include "main.h"
 
-char* block;
-char* group;
+char disk_drive[MAX_BLOCKS * BLOCK_SIZE];
+int bitmap[2] = {128, 256};
+int inodes[2] = {256, 131072};
+int dbg[2] = {131072, 1310720};
+
+// [0 - 128, 128 - 256, 256 - 131072, data groups ........]
 
 int main() {
-
-    printf("Terminal\n");
-    while(1) {
-        char** args = malloc(sizeof(char*) * MAX_ARGS);
-        for(int i = 0; i < MAX_ARGS; args++)
-            *args = malloc(sizeof(char) * MAX_ARG_LEN);
-        printf(">");
-        char* input_buff = malloc(sizeof(char) * BUFF_LEN);
-        input_buff = getInput();
-        if(strcmp(input_buff, "exit\n") == 0)
-            exit(EXIT_SUCCESS);
-        int numargs = parseInput(input_buff, args);
-        free(input_buff);
-        args = realloc(args, sizeof(char*) * numargs);
-        
-        if(strcmp(args[0], "cffs") == 0) {
-            startCFFS();
-        } else {
-            if(fork() == 0) {
-               execvp(args[0], args);
-            } else {
-                wait(NULL);
-            }
-        }
-    }
+    
+    char* t = "hello";
+    disk_write(t, 25);
+    disk_read(25, 50);
 
     return 0;
 }
 
-void startCFFS() {
+void startCFFS() {}
 
-    block = malloc(sizeof(char) * BLOCK_SIZE);
-    group = malloc(sizeof(block) * GROUP_SIZE);
+char* disk_read(int start_index, int end_index) {
 
+    int diff = end_index - start_index;
+    char* temp = malloc(sizeof(char) * diff);
+    for(int i = 0; i < diff; i++)
+        temp[i] = disk_drive[start_index + i];
+    return temp;
+}
+
+void disk_write(char* content, int start_index) {
+
+    for(int i = 0; i < strlen(content); i++)
+        disk_drive[start_index + i] = content[i];
+}
+
+void partition() {
+    // dd[0] = superblock
+    // dd[1] = inode bitmap
+    // inode end = max_blocks * .1
+    // dd[2 to inode end] = inode blocks
+    //
+    disk_drive[0] = FS_ID;
+    disk_drive[4] = MAX_BLOCKS_STR;
+    disk_drive[8] = NUM_INODES;
+
+    // initialize inode bitmap
+    for(int i = 128; i < 128 * 2; i++)
+        disk_drive[i] = '\0';
+
+    // final index of less than 
+
+}
+
+void make_file() {
+    // go through bitmap, find first empty inode
+    // go to that inode
+    // and we're like bruh, this is how it do
+    int inode;
+    for(int i = bitmap[0]; i < bitmap[1]; i++)
+        for(int j = 0; j < 8; j++)
+            if(disk_drive[i] >> j & 0) {
+                inode = BLOCK_SIZE * i + j;
+                disk_drive[i] = disk_drive[i] >> j | 1;
+            }
+    disk_drive[inode] = '\0'; // explicit, but already done
+    // [sb, inbm, inode1...inode1024, dbg1...dbg1024]
+    // dbg = 1 bitmap block + 1024 data blocks
     
+
+    // go to first dbg bitmap
+    // find first free block, if first to first + len(string) is free
+    // write string to that section of memory
+    // record in list? how about notepad
+    // for(int i = dbg[0]; i < dbg[1]; i += GROUP_BM_INC)
+    //     for(int j = i; j < 128; j++)
+    //         for(int k = 0; k < 8; k++)
+    //             if(disk_drive[j] >> k & 0)
+    //                 for
+
 }
 
 char* getInput() {
--- src/main.h
+++ src/main.h
@@ -1,4 +1,15 @@
 #define BLOCK_SIZE 128 // in bytes
-#define GROUP_SIZE 1028 // in data blocks
+#define GROUP_SIZE 1024 // in data blocks
+#define MAX_BLOCKS 10240 // in blocks
+#define MAX_POINTER 1310720 // max index
+#define GROUP_BM_INC 1025 // incrementer for data group bitmaps
 
-#define FS_ID 0xf0f03410 // necessary, but useless for our case
+#define FS_ID "f0f03410" // necessary, but useless for our case
+#define MAX_BLOCKS_STR "10240"
+#define NUM_INODES "1024"
+
+void startCFFS();
+char* disk_read(int, int);
+void disk_write(char*, int);
+char* getInput();
+int parseInput(char*, char**);
